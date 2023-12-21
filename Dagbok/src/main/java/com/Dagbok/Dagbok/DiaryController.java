f0b96770a3e67796bc1db78b357179059634d452
Mrsluggan <ericflyger@gmail.com>
2023-12-21 12:19:46 +0100

    Klart

--- Dagbok/src/main/java/com/Dagbok/Dagbok/DiaryController.java
+++ Dagbok/src/main/java/com/Dagbok/Dagbok/DiaryController.java
@@ -13,18 +13,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DiaryController {
+
+    // Skapar formatering och instans av localdate
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu-MM-dd");
     LocalDate localDate = LocalDate.now();
 
     @Autowired
     private DiaryRepository diaryRepository;
 
+    // ger sidan tillgång till databasen samt retunerar templete "index"
     @GetMapping
     public String getIndex(Model model) {
         model.addAttribute("posts", diaryRepository.sortByDates());
         return "index";
     }
 
+    // Lyssnar efter två parametrar, och sedan skapar ett nytt inlägg, därefter
+    // skickar dem till databasen
     @PostMapping("/new-item")
     public String addNew(@RequestParam("title") String title, @RequestParam("text") String text) {
         Diary diary = new Diary();
@@ -36,21 +41,26 @@ public class DiaryController {
         return "redirect:/";
     }
 
+    // Lyssnar efter en parameter, sedan skickar det till metoden som sedan tar bort
+    // inlägget med samma id
     @GetMapping("/delete")
-    public String delete(@RequestParam int id) {
+    public String delete(@RequestParam Integer id) {
         diaryRepository.deleteById(id);
         System.out.println(id);
         return "redirect:/";
 
     }
 
+    // Skickar användaren till rätt inlägg, som sedan kan redigeras
     @GetMapping("/change")
-    public String change(@RequestParam int id) {
+    public String change(@RequestParam Integer id) {
         System.out.println(id);
         return "redirect:/" + id;
 
     }
 
+    // Lyssnar efter ett id, och sedan skickar två parametrar vars data ska ändra
+    // inlägget som matchar id:et
     @PostMapping("/edit")
     public String edit(@RequestParam Integer id, @RequestParam String newTitle, @RequestParam String newText) {
         System.out.println("Id: " + id);
@@ -60,6 +70,16 @@ public class DiaryController {
         return "redirect:/" + id;
     }
 
+    // Tar fram enskild inlägg med hjälp av findwithID
+    @GetMapping("/{id}")
+    public String changeDiary(@PathVariable Integer id, Model model) {
+
+        model.addAttribute("postById", diaryRepository.findWithId(id));
+        return "form";
+    }
+
+    // Lyssnar efter vilket datum den ska kolla, skickar det vidare för sedan visa
+    // det på sidan
     @PostMapping("/date")
     public String getDate(@RequestParam("date") LocalDate date, Model model) {
         String formattedDate = date.format(dtf);
@@ -69,21 +89,30 @@ public class DiaryController {
         return "index";
     }
 
-    @GetMapping("/{id}")
-    public String changeDiary(@PathVariable Integer id, Model model) {
-
-        model.addAttribute("postById", diaryRepository.findWithId(id));
-        return "form";
-    }
-
+    // Tar in två datum, skickar det vidare till databasen för sedan få tillbaka
+    // inläggen
     @PostMapping("betweenDate")
-    public String getBetweenDates(@RequestParam("firstDate") LocalDate firstDate, @RequestParam("endDate") LocalDate endDate, Model model) {
+    public String getBetweenDates(@RequestParam(value = "firstDate", required = false) LocalDate firstDate, @RequestParam(value = "endDate", required = false) LocalDate endDate, Model model) {
 
-        System.out.println(firstDate.format(dtf) + endDate.format(dtf));
-        
-        model.addAttribute("posts", diaryRepository.findBetweenDates(firstDate.format(dtf), endDate.format(dtf)));
+        if (firstDate == null && endDate != null) {
+            model.addAttribute("posts", diaryRepository.findWithDates(endDate.format(dtf)));
+
+        } else if (firstDate != null && endDate == null) {
+            model.addAttribute("posts", diaryRepository.findWithDates(firstDate.format(dtf)));
+
+        } else if (firstDate != null && endDate != null) {
+            model.addAttribute("posts", diaryRepository.findBetweenDates(firstDate.format(dtf), endDate.format(dtf)));
+        } else {
+            System.out.println("tomt");
+        }
 
         return "index";
     }
 
+    @PostMapping("search")
+    public String search(@RequestParam("search") String search, Model model) {
+        model.addAttribute("posts", diaryRepository.findWithSearch(search));
+        System.out.println(search);
+        return "index";
+    }
 }
--- Dagbok/src/main/java/com/Dagbok/Dagbok/DiaryRepository.java
+++ Dagbok/src/main/java/com/Dagbok/Dagbok/DiaryRepository.java
@@ -1,8 +1,7 @@
 package com.Dagbok.Dagbok;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,19 +14,31 @@ public interface DiaryRepository extends CrudRepository<Diary, Integer> {
 
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
+    // Sorterar Listan, så tidigaste inläggen kommer först
+
     @Query("SELECT d FROM Diary d ORDER BY d.date")
     List<Diary> sortByDates();
 
-
+    // Tar in en parameter "Date" och letar i databasen med dom inläggen med samma
+    // datum, sedan lägger dem i en lista
     @Query("SELECT d FROM Diary d WHERE d.date = :date")
     List<Diary> findWithDates(@Param("date") String date);
 
+    // Tar in parameter, och sedan letar i databasen med inläggen med samma id,
+    // lägger dem sedan i en lista
     @Query("SELECT d FROM Diary d WHERE d.id = :id")
     List<Diary> findWithId(@Param("id") Integer id);
 
+    // Letar igenom databasen med två parameter, den börjar på startdate, och sedan
+    // enddate
     @Query("SELECT d FROM Diary d WHERE d.date BETWEEN :startDate AND :endDate ")
     List<Diary> findBetweenDates(@Param("startDate") String startDate, @Param("endDate") String endDate);
 
+    @Query("SELECT d FROM Diary d WHERE d.text LIKE %:search%")
+    List<Diary> findWithSearch(@Param("search") String search);
+
+    // Letar efter inlägget med samma id, sedan uppdateras deras värde med det som
+    // följer med i parameterna
     @Transactional
     @Modifying
     @Query("UPDATE Diary d SET d.title = :newTitle, d.text = :newText WHERE d.id = :id")
